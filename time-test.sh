9ad97b13910fd81f32cb2cad68bef23ff55cc805
anandha0807 <aduraisamy@fleetstudio.com>
2021-01-21 02:10:01 -0500

    t

--- time-test.sh
+++ time-test.sh
@@ -14,11 +14,16 @@ echo "Processing ge-"$name"_org.csv file"
 #ge-{AccountGroupName}-organizationID 
 res1=$(date +%s.%N)
 res11=$(date +%s.%N)
-sqlcmd -S PRD-DB-02.ics.com -U sa -P 'SQL h@$ N0 =' -d ge -Q "set nocount on;Select a.AccountGroupID, a.AccountID, '\"'"' + a.AccountName + '"'\"' as [AccountName], j.JobID, J.JobName from Job j
-inner join Account a on a.AccountID = j.OwnerAccountID
-where a.AccountGroupID = '$acc' and j.DeletedOn is null"  -s , -W -k1 > Output/ge-"$name"_org.csv
+sqlcmd -S PRD-DB-02.ics.com -U sa -P 'SQL h@$ N0 =' -d ge -Q "set nocount on;
+select distinct a.AssetID, am.AssetMetadataID,acc.AccountID  from AccountGroup ag
+inner join Account acc on acc.AccountGroupID = ag.AccountGroupID
+inner join Job j on j.OwnerAccountID = acc.AccountID
+inner join Asset a on a.JobID = j.JobiD
+inner join AssetMetadata am on am.AssetID = a.AssetID
+where ag.AccountGroupID = '$acc' and a.DeletetedOn is null and j.DeletedOn is null
+AND am.MetadataPropertyID  NOT IN (60,62,63,64,65,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,13043,13046,13047)" -s , -W -k1 > Output/ge-"$name"_asset_metadataid_mapped.csv
 
-sed -e 's/-,//g;s/-//g;s/,,//g;/^$/d' Output/ge-ge-"$name"_org.csv > Final_CSV/ge-"$name"_org.csv
+sed -e 's/-,//g;s/-//g;s/,,//g;/^$/d' Output/ge-"$name"_asset_metadataid_mapped.csv > Final_CSV/ge-"$name"_asset_metadataid_mapped.csv
 
 res2=$(date +%s.%N)
 dt=$(echo "$res2 - $res1" | bc)
