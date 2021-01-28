47afebb69edcc096a8a6ebdcf1e577813a90af86
anandha0807 <aduraisamy@fleetstudio.com>
2021-01-27 23:08:58 -0500

    update

---
+++ accountfind.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter the AccountGroupID: "
+read agid
+
+mkdir -p -v Output/"$agid"
+
+mkdir -p -v Final_CSV/"$agid"
+
+sqlcmd -S PRD-DB-02.ics.com -U sa -P 'SQL h@$ N0 =' -d ge -Q "set nocount on;
+
+select a.AccountID from Account a where a.AccountGroupID= '$agid'
+AND a.AccountTypeID=1 AND a.IsBillable=1 AND a.DeletedOn is NULL" -s , -W -k1 > Output/"$agid"/ge-"$agid"_accountid.csv
+
+sed -e 's/-,//g;s/-//g;s///g;/^$/d' Output/"$agid"/ge-"$agid"_accountid.csv > Final_CSV/"$agid"/ge-"$agid"_accountid.csv
+
+
